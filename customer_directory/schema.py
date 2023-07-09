1c5248b58e96845dcadc2b8b5b957385443c1604
Roza <junzhujiang@gmail.com>
2023-07-10 08:02:23 +1000

    feat: add in pagination

--- customer_directory/schema.py
+++ customer_directory/schema.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from view_customers.models import Person, Address
 
 class AddressType(DjangoObjectType):
@@ -15,10 +15,20 @@ class PersonType(DjangoObjectType):
     address = graphene.Field(AddressType)
 
 class Query(graphene.ObjectType):
-    people = graphene.List(PersonType)
-    
-    def resolve_people(root, info):
-        # We can easily optimize query count in the resolve method
-        return Person.objects.all()    
+    people = graphene.List(PersonType, page=graphene.Int(), page_size=graphene.Int())
+
+    def resolve_people(root, info, page=None, page_size=None):
+        queryset = Person.objects.all()
+
+        if page is not None and page_size is not None:
+            paginator = Paginator(queryset, page_size)
+            try:
+                people_page = paginator.page(page)
+            except (PageNotAnInteger, EmptyPage):
+                # Handle invalid page number or empty page
+                return []
+            return people_page
+        else:
+            return queryset
 
 schema = graphene.Schema(query=Query)
